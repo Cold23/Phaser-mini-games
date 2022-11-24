d1d5b2036c95b7aaa6efc7c3ce521cd697cc5e55
Cold23 <envans.doni@gmail.com>
2022-11-24 20:57:05 +0200

    revert asset path

--- components/Scenes/Preload.js
+++ components/Scenes/Preload.js
@@ -6,27 +6,27 @@ export default class Preload extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('bg', '/assets/backgroundColorForest.png')
-    this.load.spritesheet('player', '/assets/player_tilesheet.png', {
+    this.load.image('bg', 'assets/backgroundColorForest.png')
+    this.load.spritesheet('player', 'assets/player_tilesheet.png', {
       frameWidth: 80,
       frameHeight: 110,
     })
-    this.load.image('grassLeft', '/assets/grassLeft.png')
-    this.load.image('grassMid', '/assets/grassMid.png')
-    this.load.image('grassRight', '/assets/grassRight.png')
-    this.load.image('signLeft', '/assets/signLeft.png')
-    this.load.image('signRight', '/assets/signRight.png')
+    this.load.image('grassLeft', 'assets/grassLeft.png')
+    this.load.image('grassMid', 'assets/grassMid.png')
+    this.load.image('grassRight', 'assets/grassRight.png')
+    this.load.image('signLeft', 'assets/signLeft.png')
+    this.load.image('signRight', 'assets/signRight.png')
     this.load.rexWebFont({
       google: {
         families: ['Silkscreen'],
       },
     })
-    this.load.image('field', '/assets/field.png')
-    this.load.spritesheet('players', '/assets/players.png', {
+    this.load.image('field', 'assets/field.png')
+    this.load.spritesheet('players', 'assets/players.png', {
       frameWidth: 16,
       frameHeight: 16,
     })
-    this.load.image('particle', '/assets/particle.png')
+    this.load.image('particle', 'assets/particle.png')
   }
 
   create() {
