d0ae3a9d35529aa514704a76423f9bb02d795de9
Cold23 <envans.doni@gmail.com>
2022-11-24 21:09:11 +0200

    wss for https

--- components/Scenes/MiniSoccer.js
+++ components/Scenes/MiniSoccer.js
@@ -151,7 +151,7 @@ export default class MiniSoccer extends Phaser.Scene {
     this.ping = this.add.text(this.cameras.main.width - 100, 20, 'ping: 0 ms', {
       fontSize: 12,
     })
-    const client = new Colyseus.Client('ws://server-mini-game.herokuapp.com')
+    const client = new Colyseus.Client('wss://server-mini-game.herokuapp.com')
     client
       .joinOrCreate('general')
       .then((room) => {
